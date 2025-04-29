8a38ef06eeb799917ef3ac68e7e887600737ab79
robiverdev <robi.verdev1@gmail.com>
2025-04-29 23:11:15 +0200

    nuxt config changes

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-11-01",
+  future: {
+    compatibilityVersion: 4,
+  },
+  experimental: {
+    sharedPrerenderData: false,
+    compileTemplate: true,
+    resetAsyncDataToUndefined: true,
+    templateUtils: true,
+    relativeWatchPaths: true,
+    defaults: {
+      useAsyncData: {
+        deep: true,
+      },
+    },
+  },
+  unhead: {
+    renderSSRHeadOptions: {
+      omitLineBreaks: false,
+    },
+  },
+  devtools: { enabled: true },
+});
