4cc361f1c2c4993e29487559cf0a7e0921ddd920
robiverdev <robi.verdev1@gmail.com>
2025-04-30 09:03:19 +0200

    Code cleanup

---
+++ app/components/BaseButton.vue
@@ -0,0 +1,15 @@
+<template>
+  <NuxtLink
+    :to="to"
+    class="px-4 py-2 text-white self-start bg-dodgeroll-gold-500 rounded-md text-base lg:text-lg cursor-pointer"
+  >
+    <slot>{{ label }}</slot>
+  </NuxtLink>
+</template>
+
+<script setup lang="ts">
+  defineProps<{
+    label?: string;
+    to?: string;
+  }>();
+</script>
